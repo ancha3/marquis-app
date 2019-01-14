1afc7e6eba0fce7ffe5d4e036cc5301093c77dcb
Ashwin Nair <ancha3@gmail.com>
2019-01-14 14:16:55 +0100

    Initial authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule} from '@angular/fire/firestore';
+import { AngularFireAuthModule} from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './components';
+
+import { AuthService } from './services';
+
+const config = {
+  apiKey: 'AIzaSyCPAgxBZffWJ52nBNYB2Rz-mQOQlkJmZVQ',
+  authDomain: 'marquis-e2970.firebaseapp.com',
+  databaseURL: 'https://marquis-e2970.firebaseio.com',
+  projectId: 'marquis-e2970',
+  storageBucket: 'marquis-e2970.appspot.com',
+  messagingSenderId: '502258135589'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
